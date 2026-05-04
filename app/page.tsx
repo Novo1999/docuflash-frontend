1bcc6d17b1f466960ef44ba104ab8bdd5151ec72
Novo1999 <neptune.navo@gmail.com>
2026-05-05 00:03:11 +0600

    feat: add RecentUploads component to LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/landing/Footer'
 import HowItWorks from '@/components/landing/HowItWorks'
 import Navbar from '@/components/landing/Navbar'
+import { RecentUploads } from '@/components/landing/RecentUploads'
 import { UploadSection } from '@/components/landing/UploadSection'
 
 export default function LandingPage() {
@@ -11,6 +12,7 @@ export default function LandingPage() {
       <div className="max-w-[680px] mx-auto pt-[72px] pb-[64px] px-4 text-center">
         <div className="flex flex-col gap-5">
           <UploadSection />
+          <RecentUploads />
           <HowItWorks />
         </div>
       </div>
