7f961f19f33313fde6d1eac9b00db9875da23253
Novo1999 <neptune.navo@gmail.com>
2026-07-27 22:40:47 +0600

    feat: integrate Sentry for error tracking and performance monitoring
    - Added @sentry/nextjs dependency to package.json
    - Created sentry.edge.config.ts for edge feature initialization
    - Created sentry.server.config.ts for server-side initialization

---
+++ instrumentation-client.ts
@@ -0,0 +1,34 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://93e77e1c13f746202461d8cf57a86566@o4511808157384704.ingest.us.sentry.io/4511808165904384',
+
+  // Add optional integrations for additional features
+  integrations: [Sentry.replayIntegration()],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: process.env.NODE_ENV === 'development' ? 1 : 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+})
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
